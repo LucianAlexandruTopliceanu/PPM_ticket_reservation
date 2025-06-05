0bdca9ca8ced0d707c0da3caea2f5b79438c90ee
Lucian Alexandru Topliceanu <alexandru.lucian.topliceanu@gmail.com>
2025-06-05 15:15:18 +0200

    Fix serializers. Aggiunta funzionalita ad utenti e eventi

--- tickets/serializers.py
+++ tickets/serializers.py
@@ -1,20 +1,92 @@
 from rest_framework import serializers
 from .models import Event, Reservation
 from users.models import CustomUser
+from django.utils import timezone
+
 
 class EventSerializer(serializers.ModelSerializer):
+    organizer = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=CustomUser.objects.all(),
+        default=serializers.CurrentUserDefault()
+    )
+    is_past = serializers.SerializerMethodField()
+    available_seats = serializers.IntegerField(read_only=True)
+
     class Meta:
         model = Event
-        fields = '__all__'
+        fields = (
+            'id', 'title', 'description', 'date', 'location',
+            'total_seats', 'available_seats', 'organizer', 'is_past'
+        )
         read_only_fields = ('organizer', 'available_seats')
 
+    def get_is_past(self, obj):
+        return obj.date < timezone.now()
+
+    def validate_date(self, value):
+        if value < timezone.now():
+            raise serializers.ValidationError("La data dell'evento non può essere nel passato.")
+        return value
+
+    def validate_total_seats(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Il numero di posti deve essere positivo.")
+        return value
+
+
 class ReservationSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=CustomUser.objects.all(),
+        default=serializers.CurrentUserDefault()
+    )
+    event = serializers.PrimaryKeyRelatedField(queryset=Event.objects.all())
+    event_details = EventSerializer(source='event', read_only=True)
+    can_cancel = serializers.SerializerMethodField()
+
     class Meta:
         model = Reservation
-        fields = '__all__'
+        fields = (
+            'id', 'event', 'event_details', 'user', 'seats',
+            'created_at', 'is_confirmed', 'can_cancel'
+        )
         read_only_fields = ('user', 'created_at', 'is_confirmed')
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ('id', 'username', 'email')
+    def get_can_cancel(self, obj):
+        request = self.context.get('request')
+        return (
+                request and
+                request.user == obj.user and
+                obj.event.date > timezone.now()
+        )
+
+    def validate(self, data):
+        if data['event'].date < timezone.now():
+            raise serializers.ValidationError(
+                {"event": "Non è possibile prenotare per un evento passato."}
+            )
+
+        if data['seats'] <= 0:
+            raise serializers.ValidationError(
+                {"seats": "Il numero di posti deve essere positivo."}
+            )
+
+        if data['seats'] > data['event'].available_seats:
+            raise serializers.ValidationError(
+                {"seats": "Posti disponibili insufficienti."}
+            )
+
+        return data
+
+
+class EventAvailabilitySerializer(serializers.Serializer):
+    date_from = serializers.DateTimeField(required=True)
+    date_to = serializers.DateTimeField(required=True)
+
+    def validate(self, data):
+        if data['date_from'] > data['date_to']:
+            raise serializers.ValidationError(
+                "La data di inizio deve essere precedente alla data di fine."
+            )
+        return data
--- users/serializers.py
+++ users/serializers.py
@@ -1,20 +1,98 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.contrib.auth.password_validation import validate_password
+from django.core import exceptions
 
 CustomUser = get_user_model()
+
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ('id', 'username', 'email', 'phone', 'birth_date', 'is_staff')
         read_only_fields = ('is_staff',)
+        extra_kwargs = {
+            'email': {'required': True},
+            'username': {'required': True}
+        }
+
+    def validate_email(self, value):
+        if CustomUser.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Un utente con questa email esiste già.")
+        return value
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        style={'input_type': 'password'}
+    )
+    password2 = serializers.CharField(
+        write_only=True,
+        required=True,
+        style={'input_type': 'password'}
+    )
+
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'password', 'password2', 'phone', 'birth_date')
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Le password non corrispondono."})
+
+        try:
+            validate_password(attrs['password'])
+        except exceptions.ValidationError as e:
+            raise serializers.ValidationError({'password': list(e.messages)})
+
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = CustomUser.objects.create_user(**validated_data)
+        return user
+
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
-        # Aggiungi claim personalizzati
+
+        # Aggiunta claim personalizzati
         token['username'] = user.username
+        token['email'] = user.email
         token['is_staff'] = user.is_staff
+
         return token
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        # Aggiunta dati utente alla risposta
+        data['user'] = CustomUserSerializer(self.user).data
+        return data
+
+
+class PasswordResetSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+
+
+class PasswordResetConfirmSerializer(serializers.Serializer):
+    new_password = serializers.CharField(required=True)
+    new_password2 = serializers.CharField(required=True)
+    uid = serializers.CharField(required=True)
+    token = serializers.CharField(required=True)
+
+    def validate(self, attrs):
+        if attrs['new_password'] != attrs['new_password2']:
+            raise serializers.ValidationError({"new_password": "Le password non corrispondono."})
+
+        try:
+            validate_password(attrs['new_password'])
+        except exceptions.ValidationError as e:
+            raise serializers.ValidationError({'new_password': list(e.messages)})
+
+        return attrs
